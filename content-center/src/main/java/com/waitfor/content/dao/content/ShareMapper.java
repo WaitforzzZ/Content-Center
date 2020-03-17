4b5387ec4df24c2e9ba2068d1ec1b55df0262bfa
WaitforzzZ <1204918135@qq.com>
2020-03-17 19:14:03 +0800

    20200317_lombok整合内容中心

---
+++ content-center/src/main/java/com/waitfor/content/dao/content/ShareMapper.java
@@ -0,0 +1,7 @@
+package com.waitfor.content.dao.content;
+
+import com.waitfor.content.domain.entity.content.Share;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface ShareMapper extends Mapper<Share> {
+}
--- content-center/src/main/java/com/waitfor/contentcenter/TestController.java
+++ content-center/src/main/java/com/waitfor/contentcenter/TestController.java
@@ -1,27 +1,31 @@
 package com.waitfor.contentcenter;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.waitfor.contentcenter.dao.user.UserMapper;
-import com.waitfor.contentcenter.domain.entity.user.User;
+import com.waitfor.content.dao.content.ShareMapper;
+import com.waitfor.content.domain.entity.content.Share;
+
 
 @RestController
 public class TestController {
 
 	@Autowired
-	private UserMapper userMapper;
+	private ShareMapper shareMapper;
 	@GetMapping("/test")
-	public User testInsert(){
-		User user = new User();
-		user.setAvatarUrl("xxx");
-		user.setBonus(100);
-		user.setCreateTime(new Date());
-		user.setUpdateTime(new Date());
-		this.userMapper.insertSelective(user);
-		return user;
+	public List<Share> testInsert(){
+		Share build = Share.builder()
+			.auditStatus("s")
+			.author("zl")
+			.createTime(new Date())
+			.updateTime(new Date())
+			.build();
+		this.shareMapper.insertSelective(build);
+		List<Share> selectAll = this.shareMapper.selectAll();
+		return selectAll;
 	}
 }
