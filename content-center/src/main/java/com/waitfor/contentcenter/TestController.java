3fed9de6a7fc907dfa173f2c388a7b707167ee1c
WaitforzzZ <1204918135@qq.com>
2020-03-20 15:10:45 +0800

    20200320_将服务注册到nacos

--- content-center/src/main/java/com/waitfor/contentcenter/TestController.java
+++ content-center/src/main/java/com/waitfor/contentcenter/TestController.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,8 @@ public class TestController {
 
 	@Autowired
 	private ShareMapper shareMapper;
+	@Autowired
+	private DiscoveryClient discoveryClient;
 	@GetMapping("/test")
 	public List<Share> testInsert(){
 		Share build = Share.builder()
@@ -27,5 +31,16 @@ public class TestController {
 		this.shareMapper.insertSelective(build);
 		List<Share> selectAll = this.shareMapper.selectAll();
 		return selectAll;
+	}
+	
+	/**
+	 * 测试：服务发现， 证明内容中心总能找到用户中心
+	 * @return 用户中心所有实例的地址信息
+	 */
+	@GetMapping("/test2")
+	public List<ServiceInstance> getInstances(){
+		// 查询指定服务的所有实例信息
+		// consul/eureka/zookeeper...
+		return this.discoveryClient.getInstances("user-center");
 	}
 }
