97e58830adce5f4c4436e9bbea71992f0d55940e
WaitforzzZ <1204918135@qq.com>
2020-03-23 16:28:40 +0800

    20200323_使用ribbon实现负载均衡

--- content-center/src/main/java/com/waitfor/contentcenter/ContentCenterApplication.java
+++ content-center/src/main/java/com/waitfor/contentcenter/ContentCenterApplication.java
@@ -2,6 +2,7 @@ package com.waitfor.contentcenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,6 +19,8 @@ public class ContentCenterApplication {
 	// 在spring容器中， 创建一个对象， 类型RestTemplate； 名称/ID是：restTemplate
 	// <bean id="restTemplate" class="xxx.RestTemplate"/>
 	@Bean
+	//为RestTemplate整合ribbon
+	@LoadBalanced
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
--- content-center/src/main/java/com/waitfor/contentcenter/service/content/ShareService.java
+++ content-center/src/main/java/com/waitfor/contentcenter/service/content/ShareService.java
@@ -18,58 +18,63 @@ import com.waitfor.contentcenter.domain.dto.user.UserDTO;
 import com.waitfor.contentcenter.domain.entity.content.Share;
 
 import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @Service
 public class ShareService {
-	
-	@Autowired
-	private ShareMapper shareMapper;
-	@Autowired
-	private RestTemplate restTemplate;
-	@Autowired
-	private DiscoveryClient discoveryClient;
-	public ShareDTO findById(Integer id){
-		// 获取分享详情
-		Share share = this.shareMapper.selectByPrimaryKey(id);
-		// 发布人id
-		Integer userId = share.getUserId();
-		//用户中心所有实例的信息
+
+    @Autowired
+    private ShareMapper shareMapper;
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    public ShareDTO findById(Integer id) {
+        // 获取分享详情
+        Share share = this.shareMapper.selectByPrimaryKey(id);
+        // 发布人id
+        Integer userId = share.getUserId();
+        //用户中心所有实例的信息
+		/*//未接入ribbon
 		List<ServiceInstance> instances = discoveryClient.getInstances("user-center");
-		/*String targetUrl = instances.stream()
+		*//*String targetUrl = instances.stream()
 		    //数据变换
 			.map(instance ->instance.getUri().toString()+ "/users/{id}")
 			.findFirst()
-			.orElseThrow(() -> new IllegalArgumentException("当前没有实例"));*/
+			.orElseThrow(() -> new IllegalArgumentException("当前没有实例"));*//*
 		List<String> targetUrls = instances.stream()
 	    //数据变换
 		.map(instance ->instance.getUri().toString()+ "/users/{id}")
 		.collect(Collectors.toList());
 		int i = ThreadLocalRandom.current().nextInt(targetUrls.size());
 		String targetUrl = targetUrls.get(i);
-		log.info("请求的目标地址： {}",targetUrl);
-		// 通过RestTemplate调用用户微服务的/user/{userId}??
-		UserDTO userDTO = this.restTemplate.getForObject(
-				targetUrl,
-				UserDTO.class,userId);
-		//消息的装配
-		ShareDTO shareDTO = new ShareDTO();
-		BeanUtils.copyProperties(share, shareDTO);
-		shareDTO.setWxNickname(userDTO.getWxNickname());
-		return shareDTO;
-	}
+		log.info("请求的目标地址： {}",targetUrl);*/
 
-	public static void main(String[] args) {
-		RestTemplate restTemplate = new RestTemplate();
-		// 用HTTP GET方法去请求，并且返回一个对象
+        //接入ribbon
+        // 通过RestTemplate调用用户微服务的/user/{userId}??
+        UserDTO userDTO = this.restTemplate.getForObject(
+                "http://user-center/users/{userId}",
+                UserDTO.class, userId);
+        //消息的装配
+        ShareDTO shareDTO = new ShareDTO();
+        BeanUtils.copyProperties(share, shareDTO);
+        shareDTO.setWxNickname(userDTO.getWxNickname());
+        return shareDTO;
+    }
+
+    public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate();
+        // 用HTTP GET方法去请求，并且返回一个对象
 		/*String forObject=restTemplate.getForObject(
 				"http://localhost:8080/users/{id}",
 				String.class,1);
 		System.out.println(forObject);*/
-		ResponseEntity<String> forEntity = restTemplate.getForEntity(
-				"http://localhost:8080/users/{id}",
-				String.class,1);
-		System.out.println(forEntity.getBody());
-		//相比getForObject可以拿到http的状态码
-		System.out.println(forEntity.getStatusCode());
-	}
+        ResponseEntity<String> forEntity = restTemplate.getForEntity(
+                "http://localhost:8080/users/{id}",
+                String.class, 1);
+        System.out.println(forEntity.getBody());
+        //相比getForObject可以拿到http的状态码
+        System.out.println(forEntity.getStatusCode());
+    }
 }
