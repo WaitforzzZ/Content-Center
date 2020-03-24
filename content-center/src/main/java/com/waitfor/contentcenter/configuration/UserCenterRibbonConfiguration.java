7c2ff02361d244eab6f4927da16bfda8781b0aec
WaitforzzZ <1204918135@qq.com>
2020-03-24 16:42:19 +0800

    20200324_配置属性方式配置Ribbon

--- content-center/src/main/java/com/waitfor/contentcenter/configuration/UserCenterRibbonConfiguration.java
+++ content-center/src/main/java/com/waitfor/contentcenter/configuration/UserCenterRibbonConfiguration.java
@@ -3,8 +3,8 @@ package com.waitfor.contentcenter.configuration;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Configuration;
 import ribbonconfiguration.RibbonConfiguration;
-@Configuration
-@RibbonClient(name="user-center",configuration = RibbonConfiguration.class)
+//@Configuration
+//@RibbonClient(name="user-center",configuration = RibbonConfiguration.class)
 public class UserCenterRibbonConfiguration {
 
 }
