67bf98eba36c4e5e7225f6aaf1c9b85d5982f1f4
WaitforzzZ <1204918135@qq.com>
2020-03-25 08:03:26 +0800

    20200325_修改bug

--- content-center/src/main/java/ribbonconfiguration/RibbonConfiguration.java
+++ content-center/src/main/java/ribbonconfiguration/RibbonConfiguration.java
@@ -2,10 +2,10 @@ package ribbonconfiguration;
 
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@Configurable
+@Configuration
 public class RibbonConfiguration {
     @Bean
     public IRule ribbonRule() {
