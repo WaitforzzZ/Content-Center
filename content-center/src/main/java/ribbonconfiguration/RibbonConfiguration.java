00633ea337545939d6a797b8492fd7574267b45e
WaitforzzZ <1204918135@qq.com>
2020-03-27 14:57:47 +0800

    20200327_扩展Ribbon同一集群优先调用（容灾）

--- content-center/src/main/java/ribbonconfiguration/RibbonConfiguration.java
+++ content-center/src/main/java/ribbonconfiguration/RibbonConfiguration.java
@@ -2,6 +2,7 @@ package ribbonconfiguration;
 
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
+import com.waitfor.contentcenter.configuration.NacosSameClusterWeightedRule;
 import com.waitfor.contentcenter.configuration.NacosWeightedRule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,6 @@ import org.springframework.context.annotation.Configuration;
 public class RibbonConfiguration {
     @Bean
     public IRule ribbonRule() {
-        return new NacosWeightedRule();
+        return new NacosSameClusterWeightedRule();
     }
 }
