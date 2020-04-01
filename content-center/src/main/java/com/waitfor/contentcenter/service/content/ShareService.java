f8dae1ad6b8ef414d356ed3fc7daa530bd134eae
WaitforzzZ <1204918135@qq.com>
2020-04-01 10:25:22 +0800

    20200401_使用Feign实现远程http调用

--- content-center/src/main/java/com/waitfor/contentcenter/service/content/ShareService.java
+++ content-center/src/main/java/com/waitfor/contentcenter/service/content/ShareService.java
@@ -3,6 +3,7 @@ package com.waitfor.contentcenter.service.content;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.waitfor.contentcenter.feignclient.UserCenterFeignClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,7 @@ public class ShareService {
     private final ShareMapper shareMapper;
     private final RestTemplate restTemplate;
     private final DiscoveryClient discoveryClient;
+    private final UserCenterFeignClient userCenterFeignClient;
 
     public ShareDTO findById(Integer id) {
         // 获取分享详情
@@ -35,26 +37,37 @@ public class ShareService {
         // 发布人id
         Integer userId = share.getUserId();
         //用户中心所有实例的信息
-		/*//未接入ribbon
+/*		//----v1未接入ribbon
 		List<ServiceInstance> instances = discoveryClient.getInstances("user-center");
-		*//*String targetUrl = instances.stream()
+		String targetUrl = instances.stream()
 		    //数据变换
 			.map(instance ->instance.getUri().toString()+ "/users/{id}")
 			.findFirst()
-			.orElseThrow(() -> new IllegalArgumentException("当前没有实例"));*//*
+			.orElseThrow(() -> new IllegalArgumentException("当前没有实例"));
 		List<String> targetUrls = instances.stream()
 	    //数据变换
 		.map(instance ->instance.getUri().toString()+ "/users/{id}")
 		.collect(Collectors.toList());
 		int i = ThreadLocalRandom.current().nextInt(targetUrls.size());
 		String targetUrl = targetUrls.get(i);
-		log.info("请求的目标地址： {}",targetUrl);*/
+		log.info("请求的目标地址： {}",targetUrl);
+		//----未接入ribbon*/
 
-        //接入ribbon
+/*        //----v2接入ribbon 存在的问题
+        // 1. 代码不可读
+        // 2. 复杂的url难以维护：https://user-center/s?ie={ie}&f={f}&rsv_bp=1&rsv_idx=1&tn=baidu&wd=a&rsv_pq=c86459bd002cfbaa&rsv_t=edb19hb%2BvO%2BTySu8dtmbl%2F9dCK%2FIgdyUX%2BxuFYuE0G08aHH5FkeP3n3BXxw&rqlang=cn&rsv_enter=1&rsv_sug3=1&rsv_sug2=0&inputT=611&rsv_sug4=611
+        // 3. 难以相应需求的变化，变化很没有幸福感
+        // 4. 编程体验不统一
         // 通过RestTemplate调用用户微服务的/user/{userId}??
         UserDTO userDTO = this.restTemplate.getForObject(
                 "http://user-center/users/{userId}",
                 UserDTO.class, userId);
+        //----接入ribbon*/
+
+        //----v3使用Feign重构v2版本的代码，解决上述问题
+        UserDTO userDTO = this.userCenterFeignClient.findById(userId);
+        //----使用Feign重构代码
+
         //消息的装配
         ShareDTO shareDTO = new ShareDTO();
         BeanUtils.copyProperties(share, shareDTO);
