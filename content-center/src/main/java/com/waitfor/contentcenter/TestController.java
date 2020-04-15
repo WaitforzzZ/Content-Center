136e0a1b6cbcfcaa6f04b0d2950d68f49f063d1a
WaitforzzZ <1204918135@qq.com>
2020-04-15 15:59:52 +0800

    20200415_Spring Cloud Stream-编写生产者

--- content-center/src/main/java/com/waitfor/contentcenter/TestController.java
+++ content-center/src/main/java/com/waitfor/contentcenter/TestController.java
@@ -23,6 +23,8 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -190,5 +192,18 @@ public class TestController {
 				.getForObject(
 						"http://user-center/users/{userId}}",
 						UserDTO.class, userId);
+	}
+
+	@Autowired
+	private Source source;
+	@GetMapping("/test-stream")
+	public String testStream(){
+		this.source.output()
+			.send(
+					MessageBuilder
+					.withPayload("消息体")
+					.build()
+			);
+		return "success";
 	}
 }
